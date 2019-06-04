e447941e2b6574187b86baffaa8728a12e12c6d1
bob <rlp1938@gmail.com>
2019-06-05 01:54:27 +0700

    Adjustments to form field positions.

--- pe_fontfunc.sh
+++ pe_fontfunc.sh
@@ -1,16 +1,17 @@
 #!/bin/bash
 #
-# fontfunc.sh - bash function to write font data to postsript file.
+# pe_fontfunc.sh - bash function to write font data to postscript file.
 #
 # Copyright 2019 Robert L (Bob) Parker rlp1938@gmail.com
 #
+# for tm7 editing
+
 setfont()
 { # set up font specs in a postscript file
-  echo running from TM7Test
   local fn="$1"
   echo '% set up a font that may do the job' >> "$fn"
   echo /Helvetica findfont >> "$fn"
   echo '% Scale the font' >> "$fn"
-  echo 8 scalefont >> "$fn"
+  echo 9 scalefont >> "$fn"
   echo setfont >> "$fn"
 }
