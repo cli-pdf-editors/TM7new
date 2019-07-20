e091a8a73aa2f42c6fbe079bab298bb944882abe
bob <rlp1938@gmail.com>
2019-07-21 00:10:16 +0700

    1. Dropped font size to 8 from 9. 2. More portable shebang line.

--- pe_fontfunc.sh
+++ pe_fontfunc.sh
@@ -1,17 +1,15 @@
-#!/bin/bash
+#!/usr/bin/env bash
 #
 # pe_fontfunc.sh - bash function to write font data to postscript file.
 #
 # Copyright 2019 Robert L (Bob) Parker rlp1938@gmail.com
 #
-# for tm7 editing
-
 setfont()
 { # set up font specs in a postscript file
   local fn="$1"
   echo '% set up a font that may do the job' >> "$fn"
   echo /Helvetica findfont >> "$fn"
   echo '% Scale the font' >> "$fn"
-  echo 9 scalefont >> "$fn"
+  echo 8 scalefont >> "$fn"
   echo setfont >> "$fn"
 }
